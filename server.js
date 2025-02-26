8875cae2fa5ab30ce05b6885c0084431fcc7ae47
Caste <castellarosantiago@gmail.com>
2025-02-26 12:10:43 -0300

    routes fix

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Para procesar datos de formularios
 
-app.use("/api/contact", contactRoutes);
+app.use("/", contactRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,  "0.0.0.0", () => console.log(`Servidor corriendo en puerto ${PORT}`));
